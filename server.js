0518e9f67c22733d11af0439e0788b8318acd0ae
nandrea1 <nandrea1@binghamton.edu>
2013-09-03 17:46:39 -0400

    Changed some services and sockets

--- server.js
+++ server.js
@@ -1,28 +1,88 @@
 var express =require('express');
 var fs = require('fs');
+var MemoryStore = express.session.MemoryStore;
+var store = new MemoryStore();
+//var mongoose = require('mongoose');
+var filehome = "C:/nodeapps/alarm-clock/public/files/";
+var clients = {};
+var alarms = [];
+var users = {};
 var app = require('express')()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 var path = require('path');
+app.use(express.cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.cookieSession({ store: store, secret: '123456', key: 'sid' }));
 server.listen(8080);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-app.get('/page1', function (req,res){
-res.sendfile(__dirname + '/page1.html');
+app.get('/getAlarms/:userid', function (req, res){
+
+});
+
+app.get('/getAudio/:filename', function (req, res){
+var filename = req.params.filename;
+var filestring = filehome + filename;
+res.sendfile(filestring);
+});
+
+app.get('/listClients', function (req, res){
+console.log(clients);
+});
+
+app.get('/addSocket/:username/:socketid', function (req, res){
+var username = req.params.username;
+var socketid = req.params.socketid;
+var curruser = users[username];
+if(curruser == "" || curruser == undefined){
+users[username] = username;
+var socketarray = new Array();
+socketarray.push(socketid);
+users[username].sockets = socketarray;
+}
+}
+
+setInterval(function(){
+for (i=0; i<alarms.length; i++){
+var curralarm = alarms[i];
+var now = new Date();
+if(now >= curralarm.alarmtime && curralarm.set == true){
+sendAlarm(curralarm.username, curralarm);
+}
+}
+},10000);
+
+function sendAlarm(username, alarm){
+var curruser = users[username];
+for (var i=0; i<curruser.sockets; i++){
+var currsocket = curruser.sockets[i];
+currsocket.emit('alarm-event', alarm);
+}
+}
+
+app.get('/getCookie/:cookie', function (req, res){
+var cookiename = req.params.cookie;
+var cookieval = req.cookies[cookiename]
+res.send(cookieval);
 });
 
 io.set('log level', 2);
 io.sockets.on('connection', function (socket) {
+	clients[socket.id] = socket;
+	socket.emit('socket-id', {socketid: socket.id});
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
   socket.on('message', function(data){console.log("Message Sent Was: " + data);
   socket.emit('msgconfirm', 'Message successfully sent');
+  });
+  socket.on('disconnect', function() {
+    delete clients[socket.id];
   });
   socket.on('play-song', function(data){
   	var filestring = "/nodeapps/public/files" + data;
